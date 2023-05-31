4566413a93bb096aca7df3254b2de0d08bd0cadd
Tinashe Murambinda <tmurambinda01@gmail.com>
2023-05-31 06:24:55 +0200

    Style navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Products from './components/Products';
+import ErrorPage from './components/ErrorPage';
+import DiamondDrilling from './components/DiamondDrilling';
+import RockDrilling from './components/RockDrilling';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Initial</h1>
-      </header>
+    <div className="app-wrap">
+      <BrowserRouter>
+        <Navbar />
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products/diamond-drilling" element={<DiamondDrilling />} />
+          <Route path="/products/rock-drilling" element={<RockDrilling />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/RockDrilling.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RockDrilling = () => (
+    <div>
+        <h1>Rock Drilling Products</h1>
+    </div>
+);
+
+export default RockDrilling;
